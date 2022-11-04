de1d6a59015d60840aa5be4c27a4479c4d6762dd
JJW-Math <99464519+JJW-Math@users.noreply.github.com>
2022-11-03 20:20:42 -0700

    deleted a space?

--- 2022_RapidReact/src/main/java/frc/robot/subsystems/Drivebase.java
+++ 2022_RapidReact/src/main/java/frc/robot/subsystems/Drivebase.java
@@ -15,7 +15,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.DashboardStrings;
 import frc.robot.Constants.DriveConstants;
-
+  
 
 public class Drivebase extends SubsystemBase {
   private DifferentialDrive diffDrive;
